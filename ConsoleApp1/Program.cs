4644240ad176851d97dcaf5906b84c2405c927e8
mahmoudahmed13 <mahmoudahmed13@gmail.com>
2026-02-22 06:37:50 +0200

    Question01

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace ConsoleApp1
+﻿using System.Collections.Generic;
+using System.Security.Principal;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
+            #region Part 01
+
+            #region Question 01
+            //public class BankAccount
+            //{
+            //    public string Owner;
+            //    public double Balance;
+
+            //    public void Withdraw(double amount)
+            //    {
+            //        Balance -= amount;
+            //    }
+            //}
+            //a) Identify at least two problems with this design from an encapsulation perspective.
+            //1. the Field is public must be private.
+            //2. public method must be with validation(properties).
+
+            //b) Describe how you would fix this class to follow proper encapsulation principles.You do not need to write the full code.
+            //use private field and controlled access.            
+
+            //c) Explain why exposing fields directly(as public) is considered a bad practice in OOP.
+            //because public can anyone assign it any where 
+
+            #endregion
+
+            #endregion
         }
     }
 }
